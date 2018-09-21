2821ecacee3e09c830ee35f49ed6040b748798e6
linsierra <linsierra@yandex.ru>
2018-09-21 21:02:00 +0300

    Initial commit

---
+++ Templater-master/Templater/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace Templater
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tb_templ_path = new System.Windows.Forms.TextBox();
+            this.b_optempl = new System.Windows.Forms.Button();
+            this.mainpanel = new System.Windows.Forms.Panel();
+            this.b_save = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.p_fields = new System.Windows.Forms.Panel();
+            this.b_sel_excel = new System.Windows.Forms.Button();
+            this.tb_excel_path = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.mainpanel.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Файл шаблона:";
+            // 
+            // tb_templ_path
+            // 
+            this.tb_templ_path.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tb_templ_path.Location = new System.Drawing.Point(105, 10);
+            this.tb_templ_path.Name = "tb_templ_path";
+            this.tb_templ_path.Size = new System.Drawing.Size(336, 20);
+            this.tb_templ_path.TabIndex = 2;
+            this.tb_templ_path.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tb_templ_path_KeyDown);
+            // 
+            // b_optempl
+            // 
+            this.b_optempl.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.b_optempl.Location = new System.Drawing.Point(447, 8);
+            this.b_optempl.Name = "b_optempl";
+            this.b_optempl.Size = new System.Drawing.Size(75, 23);
+            this.b_optempl.TabIndex = 3;
+            this.b_optempl.Text = "Выбрать";
+            this.b_optempl.UseVisualStyleBackColor = true;
+            this.b_optempl.Click += new System.EventHandler(this.b_optempl_Click);
+            // 
+            // mainpanel
+            // 
+            this.mainpanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.mainpanel.Controls.Add(this.b_save);
+            this.mainpanel.Controls.Add(this.groupBox1);
+            this.mainpanel.Enabled = false;
+            this.mainpanel.Location = new System.Drawing.Point(16, 68);
+            this.mainpanel.Name = "mainpanel";
+            this.mainpanel.Size = new System.Drawing.Size(506, 377);
+            this.mainpanel.TabIndex = 5;
+            // 
+            // b_save
+            // 
+            this.b_save.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.b_save.Location = new System.Drawing.Point(215, 351);
+            this.b_save.Name = "b_save";
+            this.b_save.Size = new System.Drawing.Size(75, 23);
+            this.b_save.TabIndex = 6;
+            this.b_save.Text = "Выгрузить";
+            this.b_save.UseVisualStyleBackColor = true;
+            this.b_save.Click += new System.EventHandler(this.b_save_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.p_fields);
+            this.groupBox1.Location = new System.Drawing.Point(3, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(499, 342);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Соответствие полей";
+            // 
+            // p_fields
+            // 
+            this.p_fields.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.p_fields.AutoScroll = true;
+            this.p_fields.Location = new System.Drawing.Point(6, 19);
+            this.p_fields.Name = "p_fields";
+            this.p_fields.Size = new System.Drawing.Size(487, 317);
+            this.p_fields.TabIndex = 0;
+            // 
+            // b_sel_excel
+            // 
+            this.b_sel_excel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.b_sel_excel.Enabled = false;
+            this.b_sel_excel.Location = new System.Drawing.Point(447, 35);
+            this.b_sel_excel.Name = "b_sel_excel";
+            this.b_sel_excel.Size = new System.Drawing.Size(75, 23);
+            this.b_sel_excel.TabIndex = 8;
+            this.b_sel_excel.Text = "Выбрать";
+            this.b_sel_excel.UseVisualStyleBackColor = true;
+            this.b_sel_excel.Click += new System.EventHandler(this.b_sel_excel_Click);
+            // 
+            // tb_excel_path
+            // 
+            this.tb_excel_path.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tb_excel_path.Enabled = false;
+            this.tb_excel_path.Location = new System.Drawing.Point(121, 37);
+            this.tb_excel_path.Name = "tb_excel_path";
+            this.tb_excel_path.Size = new System.Drawing.Size(320, 20);
+            this.tb_excel_path.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Enabled = false;
+            this.label2.Location = new System.Drawing.Point(13, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Файл наполнения:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(534, 457);
+            this.Controls.Add(this.b_sel_excel);
+            this.Controls.Add(this.tb_excel_path);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.mainpanel);
+            this.Controls.Add(this.b_optempl);
+            this.Controls.Add(this.tb_templ_path);
+            this.Controls.Add(this.label1);
+            this.MinimumSize = new System.Drawing.Size(550, 400);
+            this.Name = "Form1";
+            this.Text = "Templater";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.mainpanel.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tb_templ_path;
+        private System.Windows.Forms.Button b_optempl;
+        private System.Windows.Forms.Panel mainpanel;
+        private System.Windows.Forms.Panel p_fields;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button b_save;
+        private System.Windows.Forms.Button b_sel_excel;
+        private System.Windows.Forms.TextBox tb_excel_path;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Templater-master/Templater/Form1.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Word = Microsoft.Office.Interop.Word;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.IO;
+
+namespace Templater
+{
+    public partial class Form1 : Form
+    {
+        Word.Application word;
+        Excel.Application excel;
+        Word.Document templ;
+        Excel.Workbook dexc;
+        List<ComboBox> inTempl = new List<ComboBox>();
+        List<ComboBox> inExcel = new List<ComboBox>();
+        public Form1()
+        {
+            InitializeComponent();
+            //tb_templ_path.Text = "templ.dot";
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            word = new Word.Application();
+            excel = new Excel.Application();
+            templ = word.Documents.Add(Environment.CurrentDirectory + "\\templ.dot");
+        }
+
+        private void cb_Select(object sender, EventArgs e)
+        {
+            /* var selitem = (sender as ComboBox).SelectedItem;
+             foreach (ComboBox cb in inTempl)
+             {
+                 if (cb == sender) continue;
+                 cb.Items.Remove(selitem);
+             }*/
+        }
+
+
+        private void loadTempl()
+        {
+            var ext = Path.GetExtension(Path.GetFullPath(tb_templ_path.Text)).ToLower();
+            if (ext != ".dot" && ext != ".dotm" && ext != ".dotx")
+            {
+                MessageBox.Show("Неверный формат файла!");
+                return;
+            }
+
+            foreach (ComboBox cb in inTempl)
+            {
+                p_fields.Controls.Remove(cb);
+            }
+
+            inTempl = new List<ComboBox>();
+
+            templ = word.Documents.Add(Path.GetFullPath(tb_templ_path.Text));
+            label2.Enabled = tb_excel_path.Enabled = b_sel_excel.Enabled = true;
+
+            List<string> bookmarks = new List<string>();
+            foreach (Word.Bookmark b in templ.Bookmarks)
+            {
+                bookmarks.Add(b.Name);
+            }
+
+
+            for (int i = 0; i < bookmarks.Count; ++i)
+            {
+                ComboBox cb = new ComboBox();
+                cb.Items.AddRange(bookmarks.ToArray());
+                cb.Top = i * (3 + cb.Height);
+                cb.SelectedValueChanged += cb_Select;
+                cb.Width = (p_fields.Width - 6) / 4 - 3;
+                cb.Anchor = AnchorStyles.Left | AnchorStyles.Top;
+
+                p_fields.Controls.Add(cb);
+                inTempl.Add(cb);
+            }
+            /*inTempl.Add(cb);
+            p_fields.Controls.Add(inTempl[1]);*/
+
+            /*for (int i = 0; i < 100; ++i)
+            {
+                TextBox tb = new TextBox();
+                p_fields.Controls.Add(tb);
+                tb.Top = i * (3 + tb.Height);
+            }*/
+        }
+
+        private void b_optempl_Click(object sender, EventArgs e)
+        {
+            if (tb_templ_path.Text.Length > 0 && File.Exists(Path.GetFullPath(tb_templ_path.Text)))
+            {
+                loadTempl();
+                return;
+            }
+
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Шаблон Word 97-2003 (*.dot)|*.dot|Шаблон Word (*.dotx)|*.dotx|Шаблон Word с поддержкой макросов (*.dotm)|*.dotm";
+            ofd.CheckPathExists = true;
+            ofd.InitialDirectory = Environment.CurrentDirectory;
+            ofd.Multiselect = false;
+            ofd.Title = "Выберите файл шаблона...";
+            var res = ofd.ShowDialog();
+            if (res == DialogResult.OK)
+            {
+                tb_templ_path.Text = ofd.FileName;
+                loadTempl();
+                return;
+            }
+
+        }
+
+        private void tb_templ_path_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                b_optempl.PerformClick();
+            }
+        }
+
+        private void b_sel_excel_Click(object sender, EventArgs e)
+        {
+            if (tb_excel_path.Text.Length > 0 && File.Exists(Path.GetFullPath(tb_excel_path.Text)))
+            {
+                loadExcel();
+                return;
+            }
+
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Книга Excel 97-2003 (*.xls)|*.xls|Книга Excel (*.xlsx)|*.xlsx";
+            ofd.CheckPathExists = true;
+            ofd.InitialDirectory = Environment.CurrentDirectory;
+            ofd.Multiselect = false;
+            ofd.Title = "Выберите файл наполнения...";
+            var res = ofd.ShowDialog();
+            if (res == DialogResult.OK)
+            {
+                tb_excel_path.Text = ofd.FileName;
+                loadExcel();
+                return;
+            }
+        }
+
+        private void loadExcel()
+        {
+            var ext = Path.GetExtension(Path.GetFullPath(tb_excel_path.Text)).ToLower();
+            if (ext != ".xls" && ext != ".xlsx")
+            {
+                MessageBox.Show("Неверный формат файла!");
+                return;
+            }
+
+            foreach (ComboBox cb in inExcel)
+            {
+                p_fields.Controls.Remove(cb);
+            }
+
+            inExcel = new List<ComboBox>();
+
+            dexc = excel.Workbooks.Open(Path.GetFullPath(tb_excel_path.Text));
+            mainpanel.Enabled = true;
+            Excel.Worksheet wex = dexc.ActiveSheet;
+            List<string> columns = new List<string>();
+            for (int i = 1; i <= 50; ++i)
+            {
+                if ((string)wex.Cells[1, i].Text == "") continue;
+                columns.Add(wex.Cells[1, i].Text);
+            }
+
+            for (int i = 0; i < inTempl.Count; ++i)
+            {
+                for (int j = 0; j < 3; ++j)
+                {
+                    ComboBox cb = new ComboBox();
+                    cb.Items.AddRange(columns.ToArray());
+                    cb.Top = i * (3 + cb.Height);
+                    cb.Left = (j + 1) * inTempl[0].Width + 3 + 4 * j;
+                    cb.SelectedValueChanged += cb_Select;
+                    cb.Width = inTempl[0].Width;
+
+                    p_fields.Controls.Add(cb);
+                    inExcel.Add(cb);
+                }
+            }
+        }
+
+        private void b_save_Click(object sender, EventArgs e)
+        {
+            /*range = templ.Bookmarks["fio"].Range;
+            range.Text = "777";
+            templ.Bookmarks.Add("fio", range);
+            range = templ.Bookmarks["fio"].Range;
+            range.Text = "666";
+            templ.Bookmarks.Add("fio", range);
+            templ.Bookmarks["rank"].Range.Text = "!!!";
+            templ.Bookmarks["univ"].Range.Text = "&&&&&&&&&&&&&&&";
+            templ.SaveAs(Environment.CurrentDirectory + "\\docs\\" + 2 + ".doc");
+            return;*/
+
+            Excel.Worksheet wex = dexc.ActiveSheet;
+            Word.Range range;
+
+            List<List<int>> cols = new List<List<int>>();
+            for (int i = 0; i < inTempl.Count(); ++i)
+            {
+                cols.Add(new List<int>());
+                for (int j = 0; j < 3; ++j)
+                {
+                    if (inExcel[i * 3 + j].SelectedIndex == -1) break;
+                    cols[i].Add(inExcel[i * 3 + j].SelectedIndex);
+                }
+            }
+
+            for (int i = 0; i < cols.Count(); ++i)
+            {
+                for (int j = 0; j < cols[i].Count; ++j)
+                {
+                    for (int z = 1; z <= 50; ++z)
+                    {
+                        if ((string)wex.Cells[1, z].Text == (string)inExcel[0].Items[cols[i][j]])
+                        {
+                            cols[i][j] = z;
+                            break;
+                        }
+                    }
+                }
+            }
+            int k = 1;
+            //templ.SaveAs(Environment.CurrentDirectory + "\\docs\\test.doc");
+            while (wex.Cells[k, 1].Text != "")
+            {
+                var ctempl = templ;
+                for (int i = 0; i < cols.Count; ++i)
+                {
+                    string tmp = "";
+                    for (int j = 0; j < cols[i].Count(); ++j)
+                    {
+                        if (j > 0)
+                            tmp += " ";
+                        tmp += wex.Cells[k, cols[i][j]].Text;
+                    }
+                    range = templ.Bookmarks[inTempl[i].SelectedItem].Range;
+                    range.Text = tmp;
+                    templ.Bookmarks.Add((string)inTempl[i].SelectedItem, range);
+                }
+                templ.SaveAs(Environment.CurrentDirectory + "\\docs\\" + k + ".doc");
+                k++;
+            }
+            MessageBox.Show("!!!");
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            templ.Close();
+            dexc.Close();
+        }
+    }
+}
